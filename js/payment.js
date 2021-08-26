546bef24088cee838978234acbf8e1b9df1c9c7a
parid-programer <73393456+parid-programer@users.noreply.github.com>
2021-08-26 15:08:41 +0200

    Add files via upload

---
+++ js/payment.js
@@ -0,0 +1,25 @@
+function submit() {
+    var input = document.getElementById("coupon").value;
+    var price = document.getElementsByClassName("price")[0];
+
+    applyCoupons()
+
+}
+
+function applyCoupons() {
+    var input = document.getElementById("coupon").value;
+    var price = document.getElementsByClassName("price")[0];
+    
+    switch (input) {
+        case "FATIME":
+            price.innerHTML = "Price: 00.00€";
+            break;
+        case "RECOMENDED":
+            price.innerHTML = "Price: 19.99€";
+            break;
+    
+        default:
+            price.innerHTML = "Price: 20.00€";
+            break;
+    }
+}
